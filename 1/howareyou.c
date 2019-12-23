73226a5f23eafceb95a0635b91e642681a016ba8
doctor608 <InFo4U2004@mail.ru>
2019-12-23 18:27:47 +0300

    First C Lesson. Compilation Stages.

---
+++ 1/howareyou.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/* Blocks:
+ *     Variables;
+ *     Constants;
+ *     Call of Function/Subprogramm/Procedure
+ *
+ *     main()
+ */
+
+int main(void)
+{
+//	printf("How are you, World?\n");
+	printf("How ");
+	printf("Are you, ");
+	printf("World?\n");
+	return 0;
+}
---
+++ 1/lol.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(void)
+{
+	printf("lol\n");
+	return 0;
+
+}
